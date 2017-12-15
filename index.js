c9a37dfdaf8ca1d40022a0934093372c53360458
bebo <bahaaharraz@gmail.com>
2017-12-15 16:09:59 +0000

    test Commit 1 for connecting to db

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ var app = express();
 app.set('view engine','ejs');
 var ejs = require('ejs');
 var path = require('path');
+var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 
 
 app.get("/index.js", function (req, res) {
-    res.render('index.ejs');
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM stocks', function(err, result) {
+            done();
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.render('index.ejs', {stocks : result.rows});
+            }
+        });
+    });
+    
 });
 
 app.listen(app.get('port'), function() {
